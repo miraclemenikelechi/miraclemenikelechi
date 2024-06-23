2b314688f958204c3d3f4fad60541c9b096c4907
miraclemenikelechi <miracle.emenike7@gmail.com>
2024-06-23 07:56:03 +0100

    :art path adjustmenst

---
+++ src/components/ui/index.ts
@@ -0,0 +1,2 @@
+export { default as NavMenuButton } from "./NavMenuButton";
+export * from "./icons";
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/miraclemenikelechi/',
-    plugins: [react()],
+    plugins: [react(),],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@styles': path.resolve(__dirname, './src/assets/styles/components'),
+        },
+    }
 });
