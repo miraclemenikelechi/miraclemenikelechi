64e7ef856b91c8b57469bce4e40b1247aa2de8b8
miraclemenikelechi <miracle.emenike7@gmail.com>
2025-01-19 07:36:37 +0100

    :art: add HTML icon component; update desktop view with animated icons; clean up unused font preconnect link

--- src/views/desktop.tsx
+++ src/views/desktop.tsx
@@ -5,29 +5,88 @@ import React from "react";
 
 import Services from "@/components/service";
 import DesktopNav from "./desktop-navbar";
+import { HTML } from "@/assets/icons";
 
 export default function Page() {
+	const backendIconsRef = React.useRef<HTMLDivElement | null>(null);
 	const backendRef = React.useRef<HTMLElement | null>(null);
+	const frontendIconsRef = React.useRef<HTMLDivElement | null>(null);
 	const frontendRef = React.useRef<HTMLElement | null>(null);
 	const textRef = React.useRef<HTMLElement | null>(null);
 
 	React.useEffect(function () {
-		gsap.timeline({
+		if (
+			!backendIconsRef.current ||
+			!backendRef.current ||
+			!frontendIconsRef.current ||
+			!frontendRef.current ||
+			!textRef.current
+		)
+			return;
+
+		// Frontend text animation
+		const frontendTextTimeline = gsap.timeline({
 			scrollTrigger: {
 				trigger: frontendRef.current,
 				start: "top center",
 				end: "bottom 60%",
 				toggleActions: "play reverse play reverse",
 			},
-		}).to(textRef.current, { text: "front", duration: 0.5, ease: "power2.out" });
+		});
+		frontendTextTimeline.to(textRef.current, {
+			text: "front",
+			duration: 0.5,
+			ease: "power2.out",
+		});
 
-		gsap.timeline({
+		// Frontend icons animation
+		const frontendIconsTimeline = gsap.timeline({
+			scrollTrigger: {
+				trigger: frontendRef.current,
+				start: "top center",
+				end: "bottom 60%",
+				toggleActions: "play reverse play reverse",
+			},
+		});
+		frontendIconsTimeline.fromTo(
+			frontendIconsRef.current.children,
+			{ opacity: 0, x: -50 },
+			{ opacity: 1, x: 0, duration: 0.5, ease: "power3.inOut", stagger: 0.05 },
+		);
+
+		// Backend text animation
+		const backendTextTimeline = gsap.timeline({
 			scrollTrigger: {
 				trigger: backendRef.current,
 				start: "top 60%",
 				toggleActions: "play reverse play reverse",
 			},
-		}).to(textRef.current, { text: "back", duration: 0.5, ease: "power2.in" });
+		});
+		backendTextTimeline.to(textRef.current, {
+			text: "back",
+			duration: 0.5,
+			ease: "power2.in",
+		});
+
+		// Backend icons animation
+		const backendIconsTimeline = gsap.timeline({
+			scrollTrigger: {
+				trigger: backendRef.current,
+				start: "top 60%",
+				toggleActions: "play reverse play reverse",
+			},
+		});
+		backendIconsTimeline.fromTo(
+			backendIconsRef.current.children,
+			{ opacity: 0, x: 50 },
+			{
+				opacity: 1,
+				x: 0,
+				duration: 0.5,
+				ease: "power3.inOut",
+				stagger: 0.05,
+			},
+		);
 	}, []);
 
 	return (
@@ -39,26 +98,50 @@ export default function Page() {
 				<DesktopNav />
 
 				<main className="relative grid grid-cols-2">
-					<aside className="sticky top-[6.75rem] p-10 h-max">
+					<aside className="sticky top-[6.75rem] p-10 h-max space-y-40">
 						<h1 className="font-black uppercase text-[10rem] leading-[8rem] text-muted-foreground">
 							<span ref={textRef}>front</span>
 							<br />
 							<span>end</span>
 						</h1>
+
+						<section className="relative">
+							<div
+								ref={frontendIconsRef}
+								className="absolute flex w-full"
+							>
+								{[...Array(5)].map((_, index) => (
+									<i
+										className="size-20"
+										key={index}
+									>
+										<HTML />
+									</i>
+								))}
+							</div>
+
+							<div
+								ref={backendIconsRef}
+								className="absolute flex w-full border"
+							>
+								{[...Array(5)].map((_, index) => (
+									<i
+										className="size-20"
+										key={index}
+									>
+										<HTML />
+									</i>
+								))}
+							</div>
+						</section>
 					</aside>
 
 					<aside className="max-h-max p-10 space-y-[10rem]">
-						<article
-							ref={frontendRef}
-							data-section="frontend"
-						>
+						<article ref={frontendRef}>
 							<Services type="frontend" />
 						</article>
 
-						<article
-							ref={backendRef}
-							data-section="backend"
-						>
+						<article ref={backendRef}>
 							<Services type="backend" />
 						</article>
 					</aside>
